e43b1316993fed4ddd2ff38652c3f14f6bd317ce
Daniagg <daniagg1506@gmail.com>
2025-02-28 02:35:03 +0700

    Update spreadtoolplus.lua

--- spreadtoolplus.lua
+++ spreadtoolplus.lua
@@ -1,3 +1,8 @@
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Weapons = ReplicatedStorage.Weapons
+
 for _, Weapon in ipairs(Weapons:GetChildren()) do
                     if Weapon:FindFirstChild("Spread") then
                         Weapon:FindFirstChild("Spread").Value += 1
