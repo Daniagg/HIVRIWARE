71f6ace8ecc296e4c41d5b3b7821aebe2da055d5
Daniagg <daniagg1506@gmail.com>
2025-02-28 02:39:49 +0700

    Update spreadtoolplus.lua

--- spreadtoolplus.lua
+++ spreadtoolplus.lua
@@ -1,13 +1,19 @@
-
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local Weapons = ReplicatedStorage.Weapons
 
 for _, Weapon in ipairs(Weapons:GetChildren()) do
-                    if Weapon:FindFirstChild("Spread") then
-                        Weapon:FindFirstChild("Spread").Value += 1
-                        for _, Spread in ipairs(Weapon:FindFirstChild("Spread"):GetChildren()) do
-                            Spread.Value += 1
-                        end
-                    end 
+    if Weapon:IsA("Folder") and Weapon:FindFirstChild("Spread") then 
+        local Spread = Weapon:FindFirstChild("Spread")
+
+        if Spread:IsA("NumberValue") then 
+            Spread.Value = Spread.Value + 1 
+
+            for _, SpreadChild in ipairs(Spread:GetChildren()) do 
+                if SpreadChild:IsA("NumberValue") then 
+                    SpreadChild.Value = SpreadChild.Value + 1 
                 end
+            end
+        end
+    end
+end
