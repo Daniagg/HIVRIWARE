12ce9b17324775408e8f73085b9bf6c3ca2461b3
Daniagg <daniagg1506@gmail.com>
2025-02-28 02:31:58 +0700

    Create spreadtoolplus.lua

---
+++ spreadtoolplus.lua
@@ -0,0 +1,8 @@
+for _, Weapon in ipairs(Weapons:GetChildren()) do
+                    if Weapon:FindFirstChild("Spread") then
+                        Weapon:FindFirstChild("Spread").Value += 1
+                        for _, Spread in ipairs(Weapon:FindFirstChild("Spread"):GetChildren()) do
+                            Spread.Value += 1
+                        end
+                    end 
+                end
